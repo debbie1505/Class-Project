d0ff97749c91f9ff26ba4352ee26e99603701f83
Taiwo Omoya <taiwoomoya272@gmail.com>
2023-11-20 16:38:01 -0500

    --This is just a really really rough draft but I fix it tmrw. Today, I just fixed the nav bar. Played around with the the color a little bit. Probably fixed that tmrw too. I did the profile card too. Would continue working on it too.Also need to work on the spacing between the two item in page. But regardless would continue working on it tmrw.

--- index.js
+++ index.js
@@ -10,13 +10,33 @@ function myFunction() {
 
   var i = 0;
 var txt = 'Welcome to CISS 100'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+var speed = 60; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
+window.onload = function typeWriter() {
   if (i < txt.length) {
     document.getElementById("header").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  }
+}
+
+//when the user scrolls the page,execute myFunction
+window.onScroll = function() {myFunction()};
+
+//Get the navbar
+var navbar = document.getElementById("navbar");
+
+//Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+//Add the sticky class to the navbar when you reach its scroll position. Remove sticky when you leave the scroll position
+function myFunction(){
+  if(window.pageYOffset >= sticky){
+    navbar.classList.add("sticky")
+  }
+  else{
+    navbar.classList.remove("sticky")
+  }
   }
 }
   
