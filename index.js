ba75ff180400694431d09ff63b5e821a5d69d138
Taiwo Omoya <taiwoomoya272@gmail.com>
2023-11-26 21:35:18 -0500

    Media Queries(mobile s,m,l):Done Currently working media query for each of the screen sizes. Done with the media query for mobile small, medium, and large.
    Notes/reflection:
    Javascript can overwrite css. Today, I was running into a problem in which
    navlinks were not showing as the css property for the nav bar was still at
    display:none even though I made it display:block. But I fixed it by adding
    an additional if statement to the myFunction() and made it match the media
    query by making it work if the width is less that 568px.
    
    Tmrw task:
    media queries for Tablet 768px

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Toggle between showing nav list and display none//
 function myFunction() {
+  if (window.innerWidth <= 568){
     var x = document.getElementById("navlinks");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -7,6 +8,7 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+}
 
   var i = 0;
 var txt = 'Welcome to CISS 100'; /* The text */
@@ -27,14 +29,5 @@ var navbar = document.getElementById("navbar");
 //Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-//Add the sticky class to the navbar when you reach its scroll position. Remove sticky when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  }
-  else {
-    navbar.classList.remove("sticky");
-  }
-  }  
 
   
